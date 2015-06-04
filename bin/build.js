fab61f18e4aff860f8556b0797287b1a0ed10cb0
Se-Won Kim <kim.sewon@gmail.com>
2015-06-04 17:49:02 +0900

    Add build and test run codes

---
+++ bin/build.js
@@ -0,0 +1,15 @@
+var fs = require('fs'), path = require('path');
+var stream = require('stream');
+
+var browserify = require('browserify');
+var babelify = require('babelify').configure({loose: 'all'});
+
+process.chdir(path.resolve(__dirname, '..'));
+
+// build for web
+browserify({standalone: 'YAtern', debug: true})
+    .transform(babelify)
+    .require('./lib/infer.js', {entry: true})
+    .bundle()
+    .on('error', function (err) { console.log('Error: ' + err.message) })
+    .pipe(fs.createWriteStream('dist/yatern.js'))
