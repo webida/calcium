30ba982653ac52ec1d32a5791f16d1b6f0efab94
Se-Won Kim <kim.sewon@gmail.com>
2015-06-08 18:03:09 +0900

    A simple web page using YAtern as a worker

--- web_tests/test.js
+++ web_tests/test.js
@@ -1,5 +1,20 @@
-var data = 'x=1;';
+$(document).ready(function () {
+    var code = document.querySelector('#code');
+    var types = document.querySelector('#types');
+    var props = document.querySelector('#props');
 
-YAtern.analyze(data);
+    if (!!window.Worker) {
+        var worker = new Worker('worker.js');
+        code.onchange = code.onkeyup = function () {
+            console.log('Message posted to worker');
+            worker.postMessage({code: code.value});
+        };
 
-console.log('hi');
+        worker.onmessage = function (e) {
+            console.log(e);
+            types.textContent = 'x\'s type names: ' + e.data.typeNames;
+            props.textContent = 'x\'s property names: ' + e.data.propNames;
+            console.log('Message received from worker');
+        };
+    }
+});
