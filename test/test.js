2ffb4780ae0bb06de59c09608c7ca35944a8cd72
Se-Won Kim <kim.sewon@gmail.com>
2015-06-22 13:08:14 +0900

    [TASK] Use Set for types and adjust TCs. It also includes some style fixes.

--- test/test.js
+++ test/test.js
@@ -7,12 +7,20 @@ var infer = require('../lib/infer');
 var types = require('../lib/domains/types');
 
 function hasTypes(aval, types) {
-    expect(aval).to.have.property('types').with.length(types.length);
-    types.forEach(function (type) {
-        expect(aval.types).to.include(type);
+    expect(aval.types.size).to.be.equal(types.length);
+    aval.types.forEach(function (type) {
+        expect(types).to.include(type);
     });
 }
 
+function getIfSingleton(set) {
+    expect(set.size).to.be.equal(1);
+    var elt = null;
+    set.forEach(function (e) {
+       elt = e;
+    });
+    return elt;
+}
 
 describe('YAtern', function () {
 
@@ -21,7 +29,7 @@ describe('YAtern', function () {
         var gObject = infer.analyze(data);
 
         hasTypes(gObject.getProp('x', true), [types.PrimNumber]);
-    })
+    });
 
     it('should analyze 02.js successfully', function () {
         var data = fs.readFileSync('./testcases/02.js').toString();
@@ -29,7 +37,7 @@ describe('YAtern', function () {
 
         hasTypes(gObject.getProp('x', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('y', true), [types.PrimBoolean]);
-    })
+    });
 
     it('should analyze 03.js successfully', function () {
         var data = fs.readFileSync('./testcases/03.js').toString();
@@ -37,7 +45,7 @@ describe('YAtern', function () {
 
         hasTypes(gObject.getProp('i', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('j', true), [types.PrimNumber]);
-    })
+    });
 
     it('should analyze 04.js successfully', function () {
         var data = fs.readFileSync('./testcases/04.js').toString();
@@ -46,14 +54,14 @@ describe('YAtern', function () {
         hasTypes(gObject.getProp('i', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('j', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('pairs', true), [types.PrimString]);
-    })
+    });
 
     it('should analyze 05.js successfully', function () {
         var data = fs.readFileSync('./testcases/05.js').toString();
         var gObject = infer.analyze(data);
 
         hasTypes(gObject.getProp('isEven', true), [types.PrimBoolean]);
-    })
+    });
 
     it('should analyze 06.js successfully', function () {
         var data = fs.readFileSync('./testcases/06.js').toString();
@@ -73,7 +81,7 @@ describe('YAtern', function () {
         hasTypes(gObject.getProp('y5', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('y6', true), [types.PrimBoolean]);
         hasTypes(gObject.getProp('y7', true), [types.PrimBoolean]);
-    })
+    });
 
     it('should analyze 07.js successfully', function () {
         var data = fs.readFileSync('./testcases/07.js').toString();
@@ -84,7 +92,7 @@ describe('YAtern', function () {
         hasTypes(gObject.getProp('x2', true),
                  [types.PrimNumber, types.PrimString]);
         hasTypes(gObject.getProp('x3', true), [types.PrimBoolean]);
-    })
+    });
 
     it('should analyze 08.js successfully', function () {
         var data = fs.readFileSync('./testcases/08.js').toString();
@@ -92,17 +100,16 @@ describe('YAtern', function () {
 
         hasTypes(gObject.getProp('one', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('ab', true), [types.PrimString]);
-    })
+    });
 
     it('should analyze 09.js successfully', function () {
         var data = fs.readFileSync('./testcases/09.js').toString();
         var gObject = infer.analyze(data);
 
-        var aval_foo = gObject.getProp('foo', true);
-        expect(aval_foo).to.have.property('types').with.length(1);
-        hasTypes(aval_foo.types[0].getProp('data', true), [types.PrimString]);
+        var type = getIfSingleton(gObject.getProp('foo', true).types);
+        hasTypes(type.getProp('data', true), [types.PrimString]);
         hasTypes(gObject.getProp('y', true), [types.PrimString]);
-    })
+    });
 
     it('should analyze 10.js successfully', function () {
         var data = fs.readFileSync('./testcases/10.js').toString();
@@ -110,9 +117,7 @@ describe('YAtern', function () {
 
         hasTypes(gObject.getProp('v', true), [types.PrimNumber]);
 
-        var aval_obj = gObject.getProp('obj', true);
-        expect(aval_obj).to.have.property('types').with.length(1);
-        var obj_type = aval_obj.types[0];
+        var obj_type = getIfSingleton(gObject.getProp('obj', true).types);
         var aval_obj_x = obj_type.getProp('x', true),
             aval_obj_z = obj_type.getProp('z', true),
             aval_obj_1 = obj_type.getProp('1', true);
@@ -120,16 +125,14 @@ describe('YAtern', function () {
         hasTypes(aval_obj_z, [types.PrimString]);
         hasTypes(aval_obj_1, [types.PrimNumber]);
 
-        var aval_arr = gObject.getProp('arr', true);
-        expect(aval_arr).to.have.property('types').with.length(1);
-        var arr_type = aval_arr.types[0];
+        var arr_type = getIfSingleton(gObject.getProp('arr', true).types);
         hasTypes(arr_type.getProp('3', true), [types.PrimString]);
 
         hasTypes(gObject.getProp('n', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('b', true), [types.PrimBoolean]);
         hasTypes(gObject.getProp('s', true), [types.PrimString]);
         hasTypes(gObject.getProp('l', true), [types.PrimNumber]);
-    })
+    });
 
     it('should analyze 11.js successfully', function () {
         var data = fs.readFileSync('./testcases/11.js').toString();
@@ -137,7 +140,7 @@ describe('YAtern', function () {
 
         hasTypes(gObject.getProp('a', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('b', true), [types.PrimString]);
-    })
+    });
 
     it('should analyze 12.js successfully', function () {
         var data = fs.readFileSync('./testcases/12.js').toString();
@@ -149,7 +152,7 @@ describe('YAtern', function () {
         hasTypes(gObject.getProp('x4', true), [types.PrimString]);
         hasTypes(gObject.getProp('x5', true), [types.PrimBoolean]);
         hasTypes(gObject.getProp('x6', true), [types.PrimBoolean]);
-    })
+    });
 
     it('should analyze 13.js successfully', function () {
         var data = fs.readFileSync('./testcases/13.js').toString();
@@ -158,5 +161,5 @@ describe('YAtern', function () {
         hasTypes(gObject.getProp('x', true), [types.PrimBoolean]);
         hasTypes(gObject.getProp('y', true), [types.PrimNumber]);
         hasTypes(gObject.getProp('z', true), [types.PrimNumber]);
-    })
-})
+    });
+});
