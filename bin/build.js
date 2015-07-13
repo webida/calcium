677730294e565d304d8b4d948ad2f07f05c4e664
Se-Won Kim <kim.sewon@gmail.com>
2015-07-13 12:38:58 +0900

    Create ./dist if not exist in build.js

--- bin/build.js
+++ bin/build.js
@@ -6,10 +6,15 @@ var babelify = require('babelify').configure({loose: 'all'});
 
 process.chdir(path.resolve(__dirname, '..'));
 
+// make ./dist if not exist
+if (!fs.existsSync('dist')) {
+    fs.mkdirSync('dist');
+}
+
 // build for web
 browserify({standalone: 'YAtern', debug: true})
     .transform(babelify)
     .require('./lib/infer.js', {entry: true})
     .bundle()
     .on('error', function (err) { console.log('Error: ' + err.message) })
-    .pipe(fs.createWriteStream('dist/yatern.js'))
+    .pipe(fs.createWriteStream('dist/yatern.js'));
