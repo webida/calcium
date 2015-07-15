9a23a1e1e943b00c8f9f723731a653a8dc9202d5
Se-Won Kim <kim.sewon@gmail.com>
2015-07-15 15:17:53 +0900

    [TASK] Simple web demo for variable at pos

--- web_tests/test.js
+++ web_tests/test.js
@@ -2,18 +2,27 @@ $(document).ready(function () {
     var code = document.querySelector('#code');
     var types = document.querySelector('#types');
     var props = document.querySelector('#props');
+    var varAtPos = document.querySelector('#varAtPos');
 
     if (!!window.Worker) {
         var worker = new Worker('worker.js');
         code.onchange = code.onkeyup = function () {
             console.log('Message posted to worker');
-            worker.postMessage({code: code.value});
+            worker.postMessage({code: code.value, pos: code.selectionStart});
         };
 
         worker.onmessage = function (e) {
             console.log(e);
             types.textContent = 'x\'s type names: ' + e.data.typeNames;
             props.textContent = 'x\'s property names: ' + e.data.propNames;
+            if (e.data.varNameAtPos) {
+                varAtPos.textContent = 'variable at pos ' +
+                    code.selectionStart + ' is ' + e.data.varNameAtPos;
+            } else {
+                varAtPos.textContent =
+                    'no variable at pos ' + code.selectionStart;
+            }
+
             console.log('Message received from worker');
         };
     }
--- web_tests/worker.js
+++ web_tests/worker.js
@@ -1,18 +1,21 @@
 importScripts('../dist/yatern.js');
 
 addEventListener('message', function (e) {
-    var t1 = (new Date).getTime();
-    var gObject = YAtern.analyze(e.data.code);
-    var t2 = (new Date).getTime();
-    console.log("Time spent: " + (t2 - t1));
+    var t1 = (new Date()).getTime();
+    var result = YAtern.analyze(e.data.code, true);
+    var gObject = result.gObject;
+    var t2 = (new Date()).getTime();
+    console.log('Time spent: ' + (t2 - t1));
 
     var message = {};
 
     var typeNames = '';
     var propNames = '';
 
+    // find types
     var types = gObject.getProp('x').types;
 
+    // find object prop names
     types.forEach(function (type) {
         typeNames += type.name + ', ';
         if (type.props) {
@@ -21,6 +24,14 @@ addEventListener('message', function (e) {
             });
         }
     });
+
+    var varNodeAtPos = YAtern.findIdentifierNodeAt(result.AST, e.data.pos);
+    if (varNodeAtPos) {
+        message.varNameAtPos = varNodeAtPos.name;
+    } else {
+        message.varNameAtPos = null;
+    }
+
     message.typeNames = typeNames;
     message.propNames = propNames;
 
